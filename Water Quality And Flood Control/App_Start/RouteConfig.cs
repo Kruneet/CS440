ca6a7a9ff143fc49ab36c688f3af5c038aed766d
Kruneet <kpate314@uic.edu>
2018-10-10 18:21:40 -0500

    Commit 2

--- Water Quality And Flood Control/App_Start/RouteConfig.cs
+++ Water Quality And Flood Control/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Water_Quality_And_Flood_Control
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "HomeTrial", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
