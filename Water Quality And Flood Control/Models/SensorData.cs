fc4bf403e64a9870a41132cea30a0be6b7e95a37
CPVirani <cviran2@uic.edu>
2018-10-10 15:45:38 -0500

    added model class

---
+++ Water Quality And Flood Control/Models/SensorData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Water_Quality_And_Flood_Control.Models
+{
+    public class SensorData
+    {
+        public String AreaName { get; set; }
+        public double Resistance { get; set; }
+        public int Voltage { get; set; }
+        public double Temperature { get; set; }
+        public int pH { get; set; }
+        public double DissolvedOxygen { get; set; }
+        public double RadioIsotopeLvl { get; set; }
+        public double StableIsotopeLvl { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public double CurrentSpeed { get; set; }
+        public double WaterLevel { get; set; }
+    }
+}
