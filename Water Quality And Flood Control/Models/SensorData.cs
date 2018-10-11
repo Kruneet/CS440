6f4b4666141f6bbc79defd8eed44c6a922b13557
Kruneet <kpate314@uic.edu>
2018-10-11 09:28:58 -0500

    Random function - Sendors data

--- Water Quality And Flood Control/Models/SensorData.cs
+++ Water Quality And Flood Control/Models/SensorData.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Water_Quality_And_Flood_Control.Models
 {
     public class SensorData
     {
-        public String AreaName { get; set; }
         public double Resistance { get; set; }
-        public int Voltage { get; set; }
+        public double Voltage { get; set; }
         public double Temperature { get; set; }
-        public int pH { get; set; }
+        public int PHValue{ get; set; }
         public double DissolvedOxygen { get; set; }
-        public double RadioIsotopeLvl { get; set; }
+        public double RadioIsotopeLevel { get; set; }
         public double StableIsotopeLvl { get; set; }
         public DateTime TimeStamp { get; set; }
         public double CurrentSpeed { get; set; }
         public double WaterLevel { get; set; }
     }
+
+   
+
 }
