bb1d31d733e50983b53f5a9b678172ea437c1e1c
CPVirani <cviran2@uic.edu>
2018-10-10 19:11:15 -0500

    just for pulling

--- Water Quality And Flood Control/Models/SensorData.cs
+++ Water Quality And Flood Control/Models/SensorData.cs
@@ -7,9 +7,14 @@ namespace Water_Quality_And_Flood_Control.Models
 {
     public class SensorData
     {
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.StringLength(10)]
         public String AreaName { get; set; }
+
         public double Resistance { get; set; }
         public int Voltage { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Range(0, 999.99)]
         public double Temperature { get; set; }
         public int pH { get; set; }
         public double DissolvedOxygen { get; set; }
