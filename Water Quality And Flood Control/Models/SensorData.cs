0784da1e901b7c628f0f6b37bea9578314cba8bb
CPVirani <cviran2@uic.edu>
2018-10-11 23:47:27 -0500

    The validation constraints were deleted and area name remived in the last merge when I pulled from repo. Did those again.

--- Water Quality And Flood Control/Models/SensorData.cs
+++ Water Quality And Flood Control/Models/SensorData.cs
@@ -1,25 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace Water_Quality_And_Flood_Control.Models
 {
     public class SensorData
     {
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Name of Location")]
+        public String AreaName { get; set; }
+
+        [Display(Name = "Resistance")]
         public double Resistance { get; set; }
-        public double Voltage { get; set; }
+
+        [Display(Name = "Voltage")]
+        public int Voltage { get; set; }
+
+        [Range(30, 40)]
+        [Display(Name = "Temperature")]
         public double Temperature { get; set; }
-        public int PHValue{ get; set; }
+
+        [Range(0, 7)]
+        [Display(Name = "pH")]
+        public int pH { get; set; }
+
+        [Display(Name = "Dissolved Oxygen")]
         public double DissolvedOxygen { get; set; }
-        public double RadioIsotopeLevel { get; set; }
-        public double StableIsotopeLvl { get; set; }
+
+        [Display(Name = "Radio Isotope Name")]
+        public String RadioIsotope { get; set; }
+
+        [Display(Name = "Stable Isotope Name")]
+        public String StableIsotope { get; set; }
+
+        [Display(Name = "TimeStamp Of Data")]
         public DateTime TimeStamp { get; set; }
+
+        [Display(Name = "Current Speed")]
         public double CurrentSpeed { get; set; }
+
+        [Range(0, 20)]
+        [Display(Name = "Water Level Above Warning Level")]
         public double WaterLevel { get; set; }
     }
-
-   
-
 }
